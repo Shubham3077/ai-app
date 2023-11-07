daa2716379ce2e918432f487bc4d580c3813dd01
Pratik Nath Tiwari <pratik@techalchemy.co>
2023-11-07 22:47:39 +0530

    Project UI finished

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+import {
+    Footer,
+    Blog,
+    Possibility,
+    Header,
+    Features,
+    WhatGPT3,
+} from "./containers";
+
+import { Cta, Brand, Navbar } from "./components";
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./cta.css";
+
+const Cta = () => {
+    return (
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-container">
+                <p>Request Early Access to Get Started</p>
+                <h2>
+                    Register today & start exploring the endless possiblities.
+                </h2>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button>Get Started</button>
+            </div>
+        </div>
+    );
+};
+
+export default Cta;
